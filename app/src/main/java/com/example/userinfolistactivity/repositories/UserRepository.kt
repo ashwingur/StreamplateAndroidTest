fdcb64bc8ccd55abed85d1dad38dca4abd1a0e35
Ashwin <ashwingur88@gmail.com>
2021-07-23 13:12:24 +1000

    Created UserRepository

---
+++ app/src/main/java/com/example/userinfolistactivity/repositories/UserRepository.kt
@@ -0,0 +1,42 @@
+package com.example.userinfolistactivity.repositories
+
+import androidx.lifecycle.LiveData
+import com.example.userinfolistactivity.api.RetrofitBuilder
+import com.example.userinfolistactivity.models.User
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+
+object UserRepository {
+
+    var job: CompletableJob? = null
+
+    fun getUsers(): LiveData<List<User>>{
+        // Initialise job
+        job = Job();
+        return object: LiveData<List<User>>(){
+            override fun onActive() {
+                // when getUsers is called
+                super.onActive()
+                job?.let {
+                    // If the job is not null then run the following code
+                    theJob ->
+                    // Creates a new coroutine on a background thread (IO dispatcher)
+                    CoroutineScope(IO + theJob).launch {
+                        val users = RetrofitBuilder.apiService.getUsers()
+                        // Now that we have the users, set it to the live data's value
+                        // However this must be done on the main thread (set live data value)
+                        withContext(Main){
+                            value = users
+                            theJob.complete()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun cancelJobs(){
+        job?.cancel()
+    }
+}
